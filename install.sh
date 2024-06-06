4f1c8a99515947c1b2c24c00a812f00e69d6860a
Ibrar Ansari <me.ibraransari@gmail.com>
2024-06-06 12:15:41 +0530

    added changes

--- install.sh
+++ install.sh
@@ -13,7 +13,18 @@ echo -e " ${GREEN}${BOLD}#======================================================
 echo -e " ${GREEN}${BOLD}#        Dotfiles installation is in progress....         #${NOCOLOR}"
 echo -e " ${GREEN}${BOLD}#=========================================================#${NOCOLOR}"
 
+# Check if the line '. ~/bin/dotfiles/bashrc' exists in ~/.bashrc, if not, append it
+if ! grep -qxF '. ~/bin/dotfiles/bashrc' ~/.bashrc; then
+    echo '. ~/bin/dotfiles/bashrc 2>/dev/null' >> ~/.bashrc
+fi
 
+# Check if the line '[ -f ~/.bashrc ] && source ~/.bashrc' exists in ~/.bash_profile, if not, append it
+if ! grep -qxF '[ -f ~/.bashrc ] && source ~/.bashrc' ~/.bash_profile; then
+    echo '[ -f ~/.bashrc ] && source ~/.bashrc' >> ~/.bash_profile
+fi
+
+# Source ~/.bash_profile to apply changes
+source ~/.bash_profile
 
 # Check Deployment status
 if [ $? -eq 0 ]
@@ -33,6 +44,9 @@ else
     echo "Installation Failed.. Please contact to Ibrar Ansari"
     exit 1
 fi
+
+# Change directory
+cd /tmp
 
 # Reload terminal
 bash
