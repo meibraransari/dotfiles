204d2acd42e1a16f8646c9da5cc23553d28122a1
Ibrar Ansari <me.ibraransari@gmail.com>
2024-06-06 11:37:17 +0530

    changes done

--- install.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-grep -qxF '. ~/bin/dotfiles/bashrc' ~/.bashrc || echo '. ~/bin/dotfiles/bashrc 2>/dev/null' >> ~/.bashrc
-grep -qxF '[ -f ~/.bashrc ] && source ~/.bashrc' ~/.bash_profile || echo '[ -f ~/.bashrc ] && source ~/.bashrc' >> ~/.bash_profile
-source ~/.bash_profile
